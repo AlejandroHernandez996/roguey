8c9308211357c8c2ba4b18024245c3b2a5d99c7d
alejandrohernandez996 <alejandrohernandez996@gmail.com>
2025-08-07 14:03:07 -0500

    init

---
+++ Source/roguey/roguey.cpp
@@ -0,0 +1,9 @@
+// Copyright Epic Games, Inc. All Rights Reserved.
+
+#include "roguey.h"
+#include "Modules/ModuleManager.h"
+
+IMPLEMENT_PRIMARY_GAME_MODULE( FDefaultGameModuleImpl, roguey, "roguey" );
+
+DEFINE_LOG_CATEGORY(Logroguey)
+ 
---
+++ Source/roguey/rogueyGameMode.cpp
@@ -0,0 +1,8 @@
+// Copyright Epic Games, Inc. All Rights Reserved.
+
+#include "rogueyGameMode.h"
+
+ArogueyGameMode::ArogueyGameMode()
+{
+	// stub
+}
